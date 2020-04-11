41236b404f78158a807594a5e84adb84d1089f6a
Kiril Dobrev <49185805+kirildobrev919@users.noreply.github.com>
2020-04-11 22:47:42 +0100

    update addDishToCours
    line 39 changed. from this._courses[courseName].push(dish);

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let menu = {
   _courses: {
-    appetizes:[],
+    appetizers:[],
     mains:[],
     desserts:[],    
   },
@@ -36,7 +36,7 @@ let menu = {
       price: dishPrice
     };
     //!!! problem no array;
-    return this._courses[courseName].push(dish);
+    return this.courseName.push(dish);
   },
   
   getRandomDishFromCourse(courseName){
@@ -53,15 +53,15 @@ let menu = {
     return `Your meal is ${appetizer.name}, ${main.name}, ${dessert.name} for the price of $${totalPrice}.`;    
   }
 }
-menu.addDishToCourse('appetizer', 'Shopska Salad', 1.00);
-menu.addDishToCourse('appetizer', 'Shepherd Salad', 2.00);
-menu.addDishToCourse('appetizer', 'Cesar Salad', 3.00);
-menu.addDishToCourse('main', 'Chicken', 5.00);
-menu.addDishToCourse('main', 'Lamb', 10.00);
-menu.addDishToCourse('main', 'Pork', 15.00);
-menu.addDishToCourse('dessert', 'Cheese cake', 5.00);
-menu.addDishToCourse('dessert', 'Caramel cream', 5.00);
-menu.addDishToCourse('dessert', 'Watermelon', 5.00);
+menu.addDishToCourse('appetizers', 'Shopska Salad', 1.00);
+menu.addDishToCourse('appetizers', 'Shepherd Salad', 2.00);
+menu.addDishToCourse('appetizers', 'Cesar Salad', 3.00);
+menu.addDishToCourse('mains', 'Chicken', 5.00);
+menu.addDishToCourse('mains', 'Lamb', 10.00);
+menu.addDishToCourse('mains', 'Pork', 15.00);
+menu.addDishToCourse('desserts', 'Cheese cake', 5.00);
+menu.addDishToCourse('desserts', 'Caramel cream', 5.00);
+menu.addDishToCourse('desserts', 'Watermelon', 5.00);
 
 let meal = this.generateRandomMeal();
 console.log(meal);
